71da59901d40a2e08eb2f65f651ac827647a6610
Sungjun24s <medires17@naver.com>
2026-06-09 20:47:32 +0900

    Refactor: Wireguard EC2용 보안그룹에 HTTP inbound추가

--- 02-security/main.tf
+++ 02-security/main.tf
@@ -35,6 +35,16 @@ resource "aws_security_group" "wg" {
     ]
   }
 
+  ingress {
+    description = "HTTP from 10.11.0.0/16"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [
+      "10.11.0.0/16"
+    ]
+  }
+
   egress {
     description = "Allow all outbound"
     from_port   = 0
