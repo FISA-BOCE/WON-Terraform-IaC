0a803633df6e8942840d10261e377af3e9fa71c3
Sungjun24s <medires17@naver.com>
2026-06-11 12:48:46 +0900

    Refactor: nginx 보안그룹 수정

--- 02-security/main.tf
+++ 02-security/main.tf
@@ -174,6 +174,16 @@ resource "aws_security_group" "nginx" {
     ]
   }
 
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
   egress {
     description = "Allow all outbound"
     from_port   = 0
